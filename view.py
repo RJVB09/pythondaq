e83a59592d8214e51c9d426b68b159adf9128dd3
RJVB09 <rjvb09@ziggo.nl>
2024-11-08 14:12:11 +0100

    Inform the user about their inputs and correct them

--- view.py
+++ view.py
@@ -1,20 +1,46 @@
 from matplotlib import pyplot as plt
 from diode_experiment import DiodeExperiment
 
+# Try to ask for an integer, ask again if the value given is not an integer.
+def try_integer_input(message):
+    while True:
+        try:
+            return int(input(message))
+        except ValueError:
+            print("Input must be an integer.")
+
+# Clamp an integer input to a certain range and notify the user about it.
+def clamp_integer_input(min, max, value):
+    if value < min:
+        print(f"Value has been set to minimum value of {min}.")
+        return min
+    elif value > max:
+        print(f"Value has been set to maximum value of {max}.")
+        return max
+
+    return value
+            
+
 # Compare two LEDs and display the results in a scatterplot
 experiment = DiodeExperiment("ASRL9::INSTR")
 
-iterations = int(input("Number of experiment iterations: "))
-start = int(input("Start measurement bound: "))
-stop = int(input("Stop measurement bound: "))
+# Ask for all measurement parameters and inform the user about their input.
+iterations = clamp_integer_input(1,50,try_integer_input("Number of experiment iterations: "))
+start = clamp_integer_input(0,1024,try_integer_input("Start measurement bound: "))
+stop = clamp_integer_input(0,1024,try_integer_input("Stop measurement bound: "))
 
+# Warn the user if the stop value is smaller than the start value
+if stop < start:
+    print("Warning: stop measurement bound is smaller than start measurement bound, no measurements will be done.")
+
+# Do the measurements
 print(input("Place LED 1, and press any key..."))
 U_1, I_1, U_1_err, I_1_err = experiment.scan(start = start, stop = stop, iterations = iterations)
 
 print(input("Place LED 2, and press any key..."))
 U_2, I_2, U_2_err, I_2_err = experiment.scan(start = start, stop = stop, iterations = iterations)
 
-
+# Display the results of both LEDs in the same graph
 plt.errorbar(x=U_1, y=I_1, yerr=I_1_err, xerr=U_1_err, fmt="ro", markersize=1.5, ecolor="r", elinewidth=0.75, label="LED 1")
 plt.errorbar(x=U_2, y=I_2, yerr=I_2_err, xerr=U_2_err, fmt="bo", markersize=1.5, ecolor="b", elinewidth=0.75, label="LED 2")
 plt.xlabel("U (V)")
