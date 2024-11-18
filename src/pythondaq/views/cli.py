e4bd0ded2b61fb1c4c4815310c7e6d3a5311db91
RJVB09 <49564898+RJVB09@users.noreply.github.com>
2024-11-18 22:18:27 +0100

    Prepared command line interface for experiment.

--- src/pythondaq/views/cli.py
+++ src/pythondaq/views/cli.py
@@ -7,24 +7,58 @@ def cmd_group():
 
 @cmd_group.command()
 def list():
+    print("list stuff")
+    pass
+
+@cmd_group.command()
+@click.argument("device")
+def info(device):
+    print(f"print identification string of {device}")
     pass
 
 @cmd_group.command()
 @click.option(
     "-vmx",
-    "--maxValue",
-    default=1023,
-    help="Maximum value in voltage range. Integer between 0-1023",
-    show_default=True,  # show default in help
+    "--maxvalue",
+    default = 1023,
+    help = "Maximum value in voltage range. Integer between 0-1023",
+    show_default = True,  # show default in help
 )
 @click.option(
     "-vmn",
-    "--minValue",
-    default=0,
-    help="Maximum value in voltage range. Integer between 0-1023",
-    show_default=True,  # show default in help
+    "--minvalue", # arguments have to be all lowercase, no camelcase
+    default = 0,
+    help = "Maximum value in voltage range. Integer between 0-1023",
+    show_default = True,  # show default in help
 )
-def scan():
+@click.option(
+    "-o",
+    "--output", # arguments have to be all lowercase, no camelcase
+    default = "",
+    help = "Output results into a csv file with a certain name. Usage: --output FILENAME",
+    show_default = True,  # show default in help
+)
+@click.option(
+    "-r",
+    "--repetitions", # arguments have to be all lowercase, no camelcase
+    default = 1,
+    help = "How many times should a measurement be repeated. Integer 0-..",
+    show_default = True,  # show default in help
+)
+@click.option(
+    "--graph/--no-graph",
+    default = False,
+    help = "Whether to show a graph or not show one.",
+    show_default = True,  # show default in help
+)
+def scan(maxvalue, minvalue, output, repetitions, graph): # Same goes for these arguments
+    print(f"scan {minvalue}-{maxvalue} stuff {repetitions} times.")
+    if output != "":
+        print(f"output file named {output}.csv")
+    
+    if graph:
+        print(f"Show a graph")
+    
     pass
 
 
