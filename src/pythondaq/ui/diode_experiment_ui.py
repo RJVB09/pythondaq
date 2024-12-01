5e324e7c1fb40b90f7dc6c7b2b0df9fd6d719914
RJVB09 <49564898+RJVB09@users.noreply.github.com>
2024-12-01 15:36:54 +0100

    Added ui styling (half done)

--- src/pythondaq/ui/diode_experiment_ui.py
+++ src/pythondaq/ui/diode_experiment_ui.py
@@ -15,18 +15,19 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
-    QLineEdit, QMainWindow, QMenuBar, QProgressBar,
-    QPushButton, QSizePolicy, QSpacerItem, QSpinBox,
-    QStatusBar, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QHBoxLayout,
+    QLabel, QLineEdit, QMainWindow, QMenuBar,
+    QProgressBar, QPushButton, QSizePolicy, QSpacerItem,
+    QSpinBox, QStatusBar, QVBoxLayout, QWidget)
 
 from pyqtgraph import PlotWidget
+import pythondaq.ui.diode_experiment_ui_resources 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(869, 567)
+        MainWindow.resize(953, 547)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout_3 = QVBoxLayout(self.centralwidget)
@@ -41,11 +42,16 @@ class Ui_MainWindow(object):
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.start_label = QLabel(self.centralwidget)
         self.start_label.setObjectName(u"start_label")
+        self.start_label.setStyleSheet(u"font: bold 15px;\n"
+"font-family: Comic Sans MS;")
 
         self.verticalLayout_2.addWidget(self.start_label)
 
         self.startSpinBox = QSpinBox(self.centralwidget)
         self.startSpinBox.setObjectName(u"startSpinBox")
+        self.startSpinBox.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 207, 226, 255), stop:1 rgba(167, 124, 255, 255));\n"
+"font-family: Comic Sans MS;\n"
+"font: bold 12px;")
         self.startSpinBox.setMaximum(1023)
 
         self.verticalLayout_2.addWidget(self.startSpinBox)
@@ -57,11 +63,16 @@ class Ui_MainWindow(object):
         self.startLayout.setObjectName(u"startLayout")
         self.stop_label = QLabel(self.centralwidget)
         self.stop_label.setObjectName(u"stop_label")
+        self.stop_label.setStyleSheet(u"font: bold 15px;\n"
+"font-family: Comic Sans MS;")
 
         self.startLayout.addWidget(self.stop_label)
 
         self.stopSpinBox = QSpinBox(self.centralwidget)
         self.stopSpinBox.setObjectName(u"stopSpinBox")
+        self.stopSpinBox.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 207, 226, 255), stop:1 rgba(167, 124, 255, 255));\n"
+"font-family: Comic Sans MS;\n"
+"font: bold 12px;")
         self.stopSpinBox.setMaximum(1023)
         self.stopSpinBox.setValue(1023)
 
@@ -77,11 +88,16 @@ class Ui_MainWindow(object):
         self.iterationsLayout.setObjectName(u"iterationsLayout")
         self.iterations_label = QLabel(self.centralwidget)
         self.iterations_label.setObjectName(u"iterations_label")
+        self.iterations_label.setStyleSheet(u"font: bold 15px;\n"
+"font-family: Comic Sans MS;")
 
         self.iterationsLayout.addWidget(self.iterations_label)
 
         self.iterationsSpinBox = QSpinBox(self.centralwidget)
         self.iterationsSpinBox.setObjectName(u"iterationsSpinBox")
+        self.iterationsSpinBox.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 207, 226, 255), stop:1 rgba(167, 124, 255, 255));\n"
+"font-family: Comic Sans MS;\n"
+"font: bold 12px;")
         self.iterationsSpinBox.setMaximum(50)
         self.iterationsSpinBox.setValue(2)
 
@@ -98,28 +114,52 @@ class Ui_MainWindow(object):
         self.deviceLayout.setObjectName(u"deviceLayout")
         self.device_label = QLabel(self.centralwidget)
         self.device_label.setObjectName(u"device_label")
+        self.device_label.setStyleSheet(u"font: bold 15px;\n"
+"font-family: Comic Sans MS;")
 
         self.deviceLayout.addWidget(self.device_label)
 
         self.updateDevicesButton = QPushButton(self.centralwidget)
         self.updateDevicesButton.setObjectName(u"updateDevicesButton")
+        self.updateDevicesButton.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(226, 0, 174, 255), stop:1 rgba(255, 219, 124, 255));\n"
+"font-family: Comic Sans MS")
 
         self.deviceLayout.addWidget(self.updateDevicesButton)
 
         self.deviceComboBox = QComboBox(self.centralwidget)
         self.deviceComboBox.setObjectName(u"deviceComboBox")
+        self.deviceComboBox.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 207, 226, 255), stop:1 rgba(167, 124, 255, 255));\n"
+"font-family: Comic Sans MS")
 
         self.deviceLayout.addWidget(self.deviceComboBox)
 
 
         self.verticalLayout.addLayout(self.deviceLayout)
 
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.MinimumExpanding)
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(10)
+        sizePolicy.setHeightForWidth(self.frame.sizePolicy().hasHeightForWidth())
+        self.frame.setSizePolicy(sizePolicy)
+        self.frame.setMinimumSize(QSize(0, 100))
+        self.frame.setStyleSheet(u"background-image: url(:/images/scrumpy.jpg);\n"
+"background-color: #cccccc; /* Used if the image is unavailable */\n"
+"height: 100px; /* You must set a specified height */\n"
+"background-position: center; /* Center the image */\n"
+"background-repeat: no-repeat; /* Do not repeat the image */\n"
+"background-size: cover; /* Resize the background image to cover the entire container */")
+        self.frame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Shadow.Raised)
 
-        self.verticalLayout.addItem(self.verticalSpacer)
+        self.verticalLayout.addWidget(self.frame)
 
         self.startButton = QPushButton(self.centralwidget)
         self.startButton.setObjectName(u"startButton")
+        self.startButton.setStyleSheet(u"\n"
+"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(57, 173, 0, 255), stop:1 rgba(212, 255, 124, 255));\n"
+"font-family: Comic Sans MS")
 
         self.verticalLayout.addWidget(self.startButton)
 
@@ -129,31 +169,42 @@ class Ui_MainWindow(object):
 
         self.saveLineEdit = QLineEdit(self.centralwidget)
         self.saveLineEdit.setObjectName(u"saveLineEdit")
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.saveLineEdit.sizePolicy().hasHeightForWidth())
-        self.saveLineEdit.setSizePolicy(sizePolicy)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.saveLineEdit.sizePolicy().hasHeightForWidth())
+        self.saveLineEdit.setSizePolicy(sizePolicy1)
+        self.saveLineEdit.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 207, 226, 255), stop:1 rgba(167, 124, 255, 255));\n"
+"font-family: Comic Sans MS;\n"
+"font: bold 11px;")
 
         self.verticalLayout.addWidget(self.saveLineEdit)
 
         self.saveButton = QPushButton(self.centralwidget)
         self.saveButton.setObjectName(u"saveButton")
+        self.saveButton.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(226, 0, 174, 255), stop:1 rgba(255, 219, 124, 255));\n"
+"font-family: Comic Sans MS")
 
         self.verticalLayout.addWidget(self.saveButton)
 
 
         self.horizontalLayout.addLayout(self.verticalLayout)
 
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
         self.verticalLayout_4 = QVBoxLayout()
         self.verticalLayout_4.setObjectName(u"verticalLayout_4")
         self.plot_widget = PlotWidget(self.centralwidget)
         self.plot_widget.setObjectName(u"plot_widget")
+        self.plot_widget.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(65, 111, 120, 255));")
 
         self.verticalLayout_4.addWidget(self.plot_widget)
 
         self.progressBar = QProgressBar(self.centralwidget)
         self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setStyleSheet(u"font: 700 11pt \"Comic Sans MS\";")
         self.progressBar.setValue(24)
 
         self.verticalLayout_4.addWidget(self.progressBar)
@@ -167,7 +218,7 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 869, 22))
+        self.menubar.setGeometry(QRect(0, 0, 953, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
